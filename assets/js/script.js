22f1639a237c39140d1558b3f29d681b07f3a543
Jerry Guerrero <jerry1996714@gmail.com>
2022-03-11 12:37:31 -0800

    changed the api and the fetch now pushes certain info to the page, also more function and stuff

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,11 +7,18 @@ var saveFavBtn = $("");
 var saveConBtn = $("");
 var artistName = $("");
 var artistBio = $("");
-var resultsPage = ("./results.html")
+var concertList = $("");
+var artistImg = $("");
+var resultsPage = ("./results.html");
 
 function artistInfo() {
-    var fmUrl = "https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=" + $(userInput).val() + "&api_key=69620325977759b14543d8cfc6fa68a0&format=json"
-    var bandsURL = "https://rest.bandsintown.com/artists/" + $(userInput).val() + "/?app_id=codingbootcamp"
+    var userRequest = localStorage.getItem("userInput")
+    var fmUrl = "https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=" + userRequest + "&api_key=69620325977759b14543d8cfc6fa68a0&format=json"
+    var bandsURL = "https://rest.bandsintown.com/artists/" + userRequest + "/?app_id=codingbootcamp"
+    var concertsURL = "https://rest.bandsintown.com/artists/" + userRequest + "/events/?app_id=codingbootcamp"
+    console.log(fmUrl)
+    console.log(bandsURL)
+    console.log(concertsURL)
 
     fetch(fmUrl)
     .then(function (response) {
@@ -23,8 +30,24 @@ function artistInfo() {
       })
       .then(function (data) {
         console.log(data);
+        artistName.text(data.artist.name)
+        artistBio.text(data.artist.bio.summary)
+
+
       });
     fetch(bandsURL)
+    .then(function (response) {
+      if (response.status === 404) {
+        console.log(response)
+      } else {
+        return response.json()
+      }
+    })
+    .then(function (data) {
+      console.log(data);
+      artistImg.attr("src", data.image_url)
+    });
+    fetch(concertsURL)
     .then(function (response) {
       if (response.status === 404) {
         console.log(response)
@@ -40,5 +63,20 @@ function artistInfo() {
 searchBtn.on("click", function(event) {
     event.preventDefault();
     event.stopPropagation();
-    artistInfo();
+    setUserInput();
+    gotoResults();
 });
+
+function setUserInput() {
+    localStorage.setItem("userInput", userInput.val().replace(/\s+/g, '+'));
+}
+
+function gotoResults() {
+    window.location.href = resultsPage
+}
+
+if (window.location.pathname == "results.html") {
+  artistInfo();
+} else if (window.location.pathname == "/C:/Users/jerry/Codingspace/All-Your-Music-Needs/results.html") {
+  artistInfo();
+}
