0884e68e7e06cad22d483a91fdad99bef06c1c8a
seanlee1379@gmail.com <seanlee1379@gmial.com>
2022-03-14 15:42:04 -0700

    more css edits

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,97 +9,115 @@ var artistName = $(".artistsName");
 var artistBio = $(".artistsBio");
 var concertList = $(".upcomingTourDates");
 var artistImg = $(".artistImage");
-var resultsPage = ("./results.html");
-var favArtists = []
+var resultsPage = "./results.html";
+var favArtists = [];
 
 function artistInfo() {
-    var userRequest = localStorage.getItem("userInput")
-    var fmUrl = "https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=" + userRequest + "&api_key=69620325977759b14543d8cfc6fa68a0&format=json"
-    var bandsURL = "https://rest.bandsintown.com/artists/" + userRequest + "/?app_id=codingbootcamp"
-    var concertsURL = "https://rest.bandsintown.com/artists/" + userRequest + "/events/?app_id=codingbootcamp"
+  var userRequest = localStorage.getItem("userInput");
+  var fmUrl =
+    "https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=" +
+    userRequest +
+    "&api_key=69620325977759b14543d8cfc6fa68a0&format=json";
+  var bandsURL =
+    "https://rest.bandsintown.com/artists/" +
+    userRequest +
+    "/?app_id=codingbootcamp";
+  var concertsURL =
+    "https://rest.bandsintown.com/artists/" +
+    userRequest +
+    "/events/?app_id=codingbootcamp";
 
-    fetch(fmUrl)
-    .then(function (response) {
-        if (response.status === 404) {
-          console.log(response)
-        } else {
-          return response.json()
-        }
-      })
-      .then(function (data) {
-        console.log(data);
-        artistName.text(data.artist.name)
-        artistBio.html(data.artist.bio.summary)
-
-
-      });
-    fetch(bandsURL)
+  fetch(fmUrl)
     .then(function (response) {
       if (response.status === 404) {
-        console.log(response)
+        console.log(response);
       } else {
-        return response.json()
+        return response.json();
       }
     })
     .then(function (data) {
       console.log(data);
-      artistImg.attr("src", data.image_url)
+      artistName.text(data.artist.name);
+      artistBio.html(data.artist.bio.summary);
     });
-    fetch(concertsURL)
+  fetch(bandsURL)
     .then(function (response) {
       if (response.status === 404) {
-        console.log(response)
+        console.log(response);
       } else {
-        return response.json()
+        return response.json();
+      }
+    })
+    .then(function (data) {
+      console.log(data);
+      artistImg.attr("src", data.image_url);
+    });
+  fetch(concertsURL)
+    .then(function (response) {
+      if (response.status === 404) {
+        console.log(response);
+      } else {
+        return response.json();
       }
     })
     .then(function (data) {
       console.log(data);
 
       for (i = 0; i < 5; i++) {
-        var date = moment(data[i].datetime).format("MMM Do, YYYY")
-        console.log(moment(date).format("MMM Do, YYYY"))
-        var li = $("<li>")
-        $("<a>").attr("href", data[i].url).text( data[0].artist.name + " Performing At " + data[i].venue.name + " " + data[i].venue.location + " " + date).appendTo(li);
+        var date = moment(data[i].datetime).format("MMM Do, YYYY");
+        console.log(moment(date).format("MMM Do, YYYY"));
+        var li = $("<li>");
+        $("<a>")
+          .attr("href", data[i].url)
+          .text(
+            data[0].artist.name +
+              " Performing At " +
+              data[i].venue.name +
+              " " +
+              data[i].venue.location +
+              " " +
+              date
+          )
+          .appendTo(li);
         li.appendTo(concertList);
       }
     });
 }
 
-searchBtn.on("click", function(event) {
-    event.preventDefault();
-    event.stopPropagation();
-    setUserInput();
-    gotoResults();
+searchBtn.on("click", function (event) {
+  event.preventDefault();
+  event.stopPropagation();
+  setUserInput();
+  gotoResults();
 });
 
 function setUserInput() {
-    localStorage.setItem("userInput", userInput.val().replace(/\s+/g, '+'));
+  localStorage.setItem("userInput", userInput.val().replace(/\s+/g, "+"));
 }
 
 function gotoResults() {
-    window.location.href = resultsPage
+  window.location.href = resultsPage;
 }
 
 function saveToFav() {
-  favArtists = JSON.parse(localStorage.getItem("FavArtist"))
-  if (favArtists == null){
-    favArtists =[]
+  favArtists = JSON.parse(localStorage.getItem("FavArtist"));
+  if (favArtists == null) {
+    favArtists = [];
   }
-  var userRequest = localStorage.getItem("userInput")
-  favArtists.push(userRequest)
-  localStorage.setItem("FavArtist", JSON.stringify(favArtists))
+  var userRequest = localStorage.getItem("userInput");
+  favArtists.push(userRequest);
+  localStorage.setItem("FavArtist", JSON.stringify(favArtists));
 }
 
 function getFav() {
-  favArtists = JSON.parse(localStorage.getItem("FavArtist"))
+  favArtists = JSON.parse(localStorage.getItem("FavArtist"));
 }
 
-saveFavBtn.on("click", function(event) {
+saveFavBtn.on("click", function (event) {
   event.preventDefault();
-    event.stopPropagation();
-    saveToFav();
-    getFav();
-})
+  event.stopPropagation();
+  saveToFav();
+  getFav();
+});
 
 getFav();
--- assets/js/upcomingconcerts.js
+++ assets/js/upcomingconcerts.js
@@ -1,27 +1,42 @@
 function concertInfo() {
-    var userRequest = localStorage.getItem("userInput")
-    var concertsURL = "https://rest.bandsintown.com/artists/" + userRequest + "/events/?app_id=codingbootcamp"
-    console.log(concertsURL)
+  var userRequest = localStorage.getItem("userInput");
+  var concertsURL =
+    "https://rest.bandsintown.com/artists/" +
+    userRequest +
+    "/events/?app_id=codingbootcamp";
+  console.log(concertsURL);
 
-  
-    fetch(concertsURL)
+  fetch(concertsURL)
     .then(function (response) {
       if (response.status === 404) {
-        console.log(response)
+        console.log(response);
       } else {
-        return response.json()
+        return response.json();
       }
     })
     .then(function (data) {
       console.log(data);
 
       for (i = 0; i < data.length && i < 20; i++) {
-          console.log(i)
-          console.log(data[i].datetime)
-        var date = moment(data[i].datetime, "YYYY MM DD").format("MMM Do, YYYY")
-        console.log(moment(date))
-        var li = $("<li>")
-        $("<a>").attr("href", data[i].url).text( data[0].artist.name + " Performing At " + data[i].venue.name + " " + data[i].venue.location + " " + date).appendTo(li);
+        console.log(i);
+        console.log(data[i].datetime);
+        var date = moment(data[i].datetime, "YYYY MM DD").format(
+          "MMM Do, YYYY"
+        );
+        console.log(moment(date));
+        var li = $("<li>");
+        $("<a>")
+          .attr("href", data[i].url)
+          .text(
+            data[0].artist.name +
+              " Performing At " +
+              data[i].venue.name +
+              " " +
+              data[i].venue.location +
+              " " +
+              date
+          )
+          .appendTo(li);
         li.appendTo(concertList);
       }
     });
